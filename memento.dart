9aa26be0dbf2e547bdd89b18406386c51c36e4d2
b1gmax <shovkoplyasmaks02@gmail.com>
2021-08-16 15:09:50 +0300

    memento

---
+++ memento.dart
@@ -0,0 +1,60 @@
+class Memento {
+  String _state;
+
+  Memento(String s) {
+    _state = s;
+    print("[Memento] State \"$s\" has been saved!");
+  }
+
+  String get state {
+    print("[Memento] Providing saved state \"$_state\"...");
+    return _state;
+  }
+}
+
+class Originator {
+  String _state;
+
+  // NOTE: This uses the state setter on init to get a handy print
+  Originator(String s) {
+    state = s;
+  }
+
+  String get state => _state;
+  void set state(String newState) {
+    _state = newState;
+    print("[Originator] Set state to $newState");
+  }
+
+  Memento saveToMemento() {
+    print("[Originator] Saving to memento...");
+    return Memento(state);
+  }
+
+  void restoreFromMemento(Memento m) {
+    print("[Originator] Restoring from memento...");
+    _state = m.state;
+    print("[Originator] Restored to $state.");
+  }
+}
+
+class CareTaker {
+  Memento memento;
+}
+
+void main() {
+  Originator me = Originator("Returned from store");
+  me.state = "Placing car keys down";
+
+  Memento locationOfKeys = me.saveToMemento();
+  CareTaker memory = CareTaker();
+  memory.memento = locationOfKeys;
+
+  me.state = "Putting away groceries";
+  me.state = "Noticed missing pickles";
+  me.state = "Looking for car keys...";
+
+  me.restoreFromMemento(memory.memento);
+  me.state = "Going back to store for pickles";
+
+}
