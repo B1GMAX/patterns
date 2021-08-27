924eabdd016bc4ab7725ca23d5613f23b2683e62
b1gmax <shovkoplyasmaks02@gmail.com>
2021-08-27 16:49:33 +0300

    new

---
+++ template_method.dart
@@ -0,0 +1,27 @@
+abstract class Abstract {
+  String abstractMethod();
+  String hookMethod() => "OMG I am a hook!";
+  void templateMethod() {
+    print(abstractMethod());
+    print(hookMethod());
+  }
+}
+
+class Concrete extends Abstract {
+  String abstractMethod() => "This is a boring example...";
+}
+
+class ConcreteOverridingHook extends Abstract {
+  String abstractMethod() => "So, so boring...";
+  @override
+  String hookMethod() => "I'm an overriden hook method!";
+}
+
+void main() {
+  var con1 = Concrete();
+  var con2 = ConcreteOverridingHook();
+  con1.templateMethod();
+  con2.templateMethod();
+
+
+}
